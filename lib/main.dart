d135dfefcad0d96fad73570ae1f5f77612574e8e
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2025-03-13 22:58:47 +0300

    add the stack widget

--- lib/main.dart
+++ lib/main.dart
@@ -96,10 +96,16 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Image.asset(
-        'assets/images/bg.jpg',
-        height: double.infinity,
-        fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/bg.jpg',
+            fit: BoxFit.cover,
+            height: 300,
+          ),
+          SizedBox(
+              height: 300, child: Center(child: Text('Flutter development')))
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
