e653e824f887e379d8fa2f9c869f08b82bdb89e8
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2025-03-28 11:13:51 +0300

    add a slider and print out the values

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -11,6 +11,7 @@ class _ProfilePageState extends State<ProfilePage> {
   TextEditingController controller = TextEditingController();
   bool? isChecked = false;
   bool isSwitched = false;
+  double sliderValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -54,13 +55,24 @@ class _ProfilePageState extends State<ProfilePage> {
             },
           ),
           SwitchListTile.adaptive(
-              title: Text('Switch me'),
-              value: isSwitched,
-              onChanged: (bool value) {
-                setState(() {
-                  isSwitched = value;
-                });
-              })
+            title: Text('Switch me'),
+            value: isSwitched,
+            onChanged: (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
+          Slider.adaptive(
+            max: 10.0,
+            value: sliderValue,
+            onChanged: (double value) {
+              setState(() {
+                sliderValue = value;
+              });
+              print(value);
+            },
+          )
         ],
       ),
     );
